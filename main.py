e74a6cba9061f03175f19b6adaa71d0f4b5f607b
Joy Jain <joyjain184@gmail.com>
2025-07-25 19:32:18 +0530

    Add root route

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from blog.routers import blog, user, authentication
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+# Create all database tables
+models.Base.metadata.create_all(engine)
 
+# Root route
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Blog API 🚀. Visit /docs for the Swagger UI."}
+
+# Include routers
 app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
